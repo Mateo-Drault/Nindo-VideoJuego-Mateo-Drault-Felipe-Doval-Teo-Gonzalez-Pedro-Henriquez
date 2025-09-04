179cc4bef94171c36936f0213cd8299543106169
Pedro <pedrohenriiquez.2008@gmail.com>
2025-09-04 09:09:06 -0300

    4/9 ehhh no se chau

---
+++ Nindo/Assets/Scripts/New Player/NewPlayerMovement.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewPlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float speed = 3f;
+    [SerializeField] private float rotationSpeed = 1.0f;
+    [SerializeField] private float maxRiseSpeed = 1;
+    [SerializeField] private float maxFallSpeed = 20;
+    [SerializeField] private Rigidbody playerRB;
+    [SerializeField] private Animator anim;
+    [SerializeField] private LockOnTarget lockOnTarget;
+    [SerializeField] private PlayerSwordAnimation playerSwordAnimation;
+    private Vector3 forward, right;
+    void Start()
+    {
+        forward = Camera.main.transform.forward;
+        forward.y = 0;
+        forward = Vector3.Normalize(forward);
+
+        right = Camera.main.transform.right;
+        right.y = 0;
+        right = Vector3.Normalize(right);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        Vector3 direction = horizontalInput * right + verticalInput * forward;
+        if (direction != Vector3.zero && !anim.GetCurrentAnimatorStateInfo(0).IsName("Stunned") && !anim.GetCurrentAnimatorStateInfo(0).IsName("Attacking") && !anim.GetCurrentAnimatorStateInfo(0).IsName("Parrying"))
+        {
+            transform.position += direction * speed * Time.deltaTime;
+            anim.SetBool("isRunning", true);
+            if (!lockOnTarget.isLocked)
+            {
+                Quaternion Target = Quaternion.LookRotation(direction);
+                transform.rotation = Quaternion.Slerp(transform.rotation, Target, rotationSpeed * Time.deltaTime);
+            }
+        }
+    }
+    void FixedUpdate()
+    {
+        Vector3 vel = playerRB.velocity;
+        vel.y = Mathf.Clamp(vel.y, -maxFallSpeed, maxRiseSpeed);
+        playerRB.velocity = vel;
+    }
+}
--- Nindo/Assets/Scripts/New Player/PlayerCombat.cs
+++ Nindo/Assets/Scripts/New Player/PlayerCombat.cs
@@ -4,15 +4,106 @@ using UnityEngine;
 
 public class PlayerCombat : MonoBehaviour
 {
-    // Start is called before the first frame update
+    //PlayerSwordAnimation cositas
+
+    public Animator animator;
+    public bool isAttacking;
+    public BoxCollider swordCollider;
+    public bool isStunned;
+
+    [Header("Attack Movement")]
+    public float attackDashSpeed = 5f;//velocidad del dash
+    public float attackDashTime = 0.15f;//duración del dash en segundos
+    private bool isDashing = false;
+
+    [SerializeField] private PlayerMovement playerMovement; //ARREGLAR
+
+    //PlayerBeingDamaged cositas
+
+    public bool hasRecievedDamage = false;
+    [SerializeField] private Renderer playerRenderer;
+    public Material originalMaterial;
+    public Material newMaterial;
+    [SerializeField] private PlayerLifeManager playerLifeManager;
+    [SerializeField] private PlayerHealthBar PlayerHealthBar;
+    [SerializeField] private float enemyDamage; //conectarlo con el enemigo de mate
+    [SerializeField] private float invincibilityDuration;
+
     void Start()
     {
-        
+        isAttacking = false;
+        PlayerHealthBar.UpdatePlayerHealthBar(playerLifeManager.actualHealth, playerLifeManager.maxHealth);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SwordHit();
+        }
+    }
+
+    public void SwordHit()
+    {
+        if (!animator.GetCurrentAnimatorStateInfo(0).IsName("Attacking"))//animación de ataque
+        {
+            animator.SetTrigger("attack");
+        }
+    }
+    public void DoAttackDash()
+    {
+        StartCoroutine(AttackDash());
+    }
+    public void StopAttacking()
+    {
+        isAttacking = false;
+        swordCollider.enabled = false;
+    }
+
+    public void StartAttaking()
+    {
+        swordCollider.enabled = true;
+        isAttacking = true;
+    }
+
+    public void InterrumptAttack()
+    {
+        if (isAttacking)
+        {
+            animator.SetTrigger("stunned");
+            animator.ResetTrigger("attack");
+            isAttacking = false;
+            swordCollider.enabled = false;
+        }
+    }
+
+    private IEnumerator AttackDash()
+    {
+        if (isDashing) yield break;//evita que se solapen dashes
+
+        isDashing = true;
+        float timer = 0f;
+
+        while (timer < attackDashTime)
+        {
+            transform.position += transform.forward * attackDashSpeed * Time.deltaTime; //ARREGLARRR
+            Debug.Log("Daleeee");
+            timer += Time.deltaTime;
+            yield return null;
+        }
+
+        isDashing = false;
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("EnemySword") && !hasRecievedDamage)
+        {
+            Debug.Log("daño");
+            playerRenderer.material = newMaterial;
+            hasRecievedDamage = true;
+            playerLifeManager.actualHealth -= enemyDamage;
+            PlayerHealthBar.UpdatePlayerHealthBar(playerLifeManager.actualHealth, playerLifeManager.maxHealth);
+            Invoke("RestartInvincibility", invincibilityDuration);
+        }
     }
 }
