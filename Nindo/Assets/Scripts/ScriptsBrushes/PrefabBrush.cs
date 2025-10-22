2555dea69b388dd2d2f3bc4ae214c671a2a805f9
Pedro <pedrohenriiquez.2008@gmail.com>
2025-10-22 10:43:04 -0300

    22/10 Prefab Brush

---
+++ Nindo/Assets/Scripts/ScriptsBrushes/PrefabBrush.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor.Tilemaps;
+
+[CreateAssetMenu(fileName = "Prefab Brush", menuName = "Assets/Brushes/Prefab Brush")]
+[CustomGridBrush(hideAssetInstances: false, hideDefaultInstance: true, defaultBrush: false, defaultName: "PrefabBrush")]
+
+public class PrefabBrush : GameObjectBrush
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
